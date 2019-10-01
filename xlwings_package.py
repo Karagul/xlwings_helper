086a97315a59f70b14b9c2dc97bf5010a93d4efc
James Kabbes <james.kabbes@gmail.com>
2019-10-01 17:10:38 -0500

    moved to cpd

--- xlwings_package.py
+++ xlwings_package.py
@@ -1,7 +1,19 @@
 import xlwings as xw
 import pandas as pd
 
+import sys
 
+try:
+    sys.path.append('''C:/Users/james/Dropbox/Python/Analytics''')
+except:
+    pass
+
+try:
+    sys.path.append('''C:/Users/e150445/Documents/Analytics''')
+except:
+    pass
+
+import CustomPandas as cpd
 
 def base_import():
 
@@ -58,9 +70,7 @@ def get_df_from_ws(ws):
     df = df_from_rows( get_rows(ws))
     return df
 
-def df_change_row_ind_col_value(df, index, column, new_val):
-    df.loc[index, column] = new_val
-    return df
+
 
 def write_df_col_to_ws(ws, df, col_index, col_name):
 
@@ -79,27 +89,6 @@ def write_df_col_to_ws(ws, df, col_index, col_name):
     write_2d(ws, values_col, top_left_cell = (1, col_index + 1))
 
 
-def value_counts_in_df(df, col):
-
-    return df[col].value_counts()
-
-def map_df_column_to_dict(df, col, dict, new_col):
-
-    df[new_col] = df[col].map(dict)
-    return df
-
-def dict_from_two_columns(df, key_col, val_col):
-
-    #In [9]: pd.Series(df.Letter.values,index=df.Position).to_dict()
-    #Out[9]: {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'}
-    a = pd.Series(df[val_col].values, index = df[key_col]).to_dict()
-    return a
-
-def map_df_col_to_new_id(df, col, new_col_name, df2, id_col, map_col):
-
-    map_dict = dict_from_two_columns(df2, id_col, map_col)
-    df = map_df_column_to_dict(df, col, map_dict, new_col_name)
-    return df
 
 def alpha_from_index(integer):
 
@@ -192,31 +181,16 @@ def sort_ws(ws, column_alphas):
 
     df = df_from_rows( get_rows(ws) )
     headers = get_column_headers_from_alpha(df, column_alphas)
-    df = sort_df(df, headers)
+    df = cpd.sort_df(df, headers)
     #print (headers)
     #print (df)
     write_df_to_ws(ws, df)
-
-def sort_df(df, columns, ascend = True, na_pos = 'last'):
-
-    df = df.sort_values(columns, ascending = ascend, na_position = na_pos)
-    return df
 
 def check_column_in_list(df, column, list, new_column):
     '''returns a dataframe with a boolean value in new column if the row had one of those value or not'''
 
     df[new_column] = df[column].isin(list)
     return df
-
-def new_df_with_value_in_col(df, col, val, opposite = False):
-
-    if not opposite:
-        new_df = df.loc[df[col] == val]
-        return new_df
-
-    if opposite:
-        new_df = df.loc[df[col] != val]
-        return new_df
 
 def add_sheet(sheet_name, work_book):
 
@@ -233,37 +207,9 @@ def combine_string_columns(df, col1, col2, new_column):
     df[new_column] = df[col1].map(str) + df[col2].map(str)
     return df
 
-def drop_these_cols(df, cols):
-
-    '''drops cols from df'''
-    return df.drop(cols, axis = 1)
-
 def get_wb(book_name):
 
     return xw.Book(book_name)
-
-def combine_all_string_columns(df, columns, new_column):
-
-    '''combines all columns contained in list found in df and renames it new column'''
-    col1 = columns[0]
-    col2 = columns[1]
-    cols_added = []
-    for i in range(len(columns) - 2):
-
-        join = 'join' + str(i)
-        cols_added.append(join)
-        df = combine_string_columns(df, col1, col2, join)
-
-        col1 = join
-        col2 = columns[2 + i]
-
-
-    #last join
-    df = combine_string_columns(df, col1, col2, new_column)
-    #print (df)
-    df = drop_these_cols(df, cols_added)
-    #print (df)
-    return df
 
 def write_df_to_ws(ws, df):
 
@@ -276,30 +222,6 @@ def write_df_to_ws(ws, df):
         write_df_col_to_ws(ws, df, col_num, col)
 
 
-def and_gate_many_cols(df, columns, col_name):
-
-    col1 = columns[0]
-    col2 = columns[1]
-    cols_added = []
-    for i in range(len(columns) - 2):
-
-        bool_num = 'bool' + str(i)
-        join = bool_num + ': ' + col1 + '/' + col2
-        cols_added.append(join)
-        df[join] = df[col1] & df[col2]
-
-        col1 = join
-        col2 = columns[2 + i]
-
-
-    df[col_name] = df[col1] & df[col2]
-    df = drop_these_cols(df, cols_added)
-    print ('cols added')
-    print (cols_added)
-    print ('after dropping')
-    #print (df)
-    return df
-
 def alpha_from_column_names(df, strings):
 
     cols = df.columns.tolist()
@@ -311,42 +233,9 @@ def alpha_from_column_names(df, strings):
 
     return alphas
 
-def move_last_column_to_first(df):
-
-    cols = df.columns.tolist()
-    cols = cols[-1:] + cols[:-1]
-    df = df[cols]
-    return df
-
-def get_column(ws, col_index, nested = True):
-
-    '''gets a column from the ws'''
-    col = []
-    if nested:
-        for row in get_rows(ws):
-            col.append([ row[col_index] ] )
-
-    else:
-        for row in get_rows(ws):
-            col.append( row[col_index] )
-    return col
 def change_cell_color(ws, top_left_cell, cell_color, bottom_right_cell = None):
 
     '''changes a range of cells a certain color'''
     if bottom_right_cell == None:
         bottom_right_cell = top_left_cell
     ws.range(top_left_cell, bottom_right_cell).color = cell_color
-
-def fill_nans(df, value_to_fill):
-
-    df = df.fillna(value_to_fill)
-    return df
-
-def get_date_and_time():
-
-    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-
-def df_replace(df, value_to_change, to_fill):
-
-    df = df.replace(value_to_change, to_fill)
-    return df
